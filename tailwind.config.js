d6c830de940c236060e64713c19befe51a664bad
jayaxsurya <jayasurya.dev@outlook.com>
2024-03-18 13:00:25 +0530

    fix:added all files

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/*.{html,js}"],
+  theme: {
+    extend:{
+      fontFamily:{
+        playfair:"'Playfair Display', serif",
+        lato:"'Lato', sans-serif"
+      }
+     },
+  },
+  plugins: [],
+}
+
+
+
